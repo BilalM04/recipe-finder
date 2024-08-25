30cdbf6056c6ff5651ae63cd4aa52030726af963
Mohammad Bilal <bilal71612@gmail.com>
2024-08-25 19:33:47 -0400

    hash routing

--- src/index.js
+++ src/index.js
@@ -4,25 +4,21 @@ import './index.css';
 import Search from './pages/Search/Search';
 import HomePage from './pages/HomePage/HomePage';
 import SavedRecipes from './pages/SavedRecipes/SavedRecipes';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: process.env.REACT_APP_FOR_PATH + "/",
-    element: <HomePage/>,
-  },
-  {
-    path: process.env.REACT_APP_FOR_PATH + "/search",
-    element: <Search/>,
-  },{
-    path: process.env.REACT_APP_FOR_PATH + "/saved",
-    element: <SavedRecipes/>,
-  },
-]);
+const App = () => (
+  <Router baseline="/">
+    <Routes>
+      <Route path={process.env.REACT_APP_FOR_PATH + "/"} element={<HomePage />} />
+      <Route path={process.env.REACT_APP_FOR_PATH + "/search"} element={<Search />} />
+      <Route path={process.env.REACT_APP_FOR_PATH + "/saved"} element={<SavedRecipes />} />
+    </Routes>
+  </Router>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>
 );
